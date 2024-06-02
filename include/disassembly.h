9bf3acd35f0c1e175520daf34f36e31cb44a0ad7
AdiKsOnDev <adilalizade13@gmail.com>
2024-06-02 22:34:37 +0400

    docs: Docstrings for disassembly

--- include/disassembly.h
+++ include/disassembly.h
@@ -2,6 +2,12 @@
 #include <windows,h>
 #include <capstone/capstone.h>
 
-
+/* Converts RVA to file offset
+ * 
+ * param: pNtHeaders, NT headers of a PE file
+ * param: rva, DWORD typed RVA to be converted
+ *
+ * return: DWORD offset
+ */
 DWORD rva_to_offset(PIMAGE_NT_HEADERS pNtHeaders, DWORD rva); 
 #endif // !DISASSEMBLY
--- src/disassembly.c
+++ src/disassembly.c
@@ -1,6 +1,12 @@
 #include "../include/disassembly.h"
 
-// Function to convert RVA to file offset
+/* Converts RVA to file offset
+ * -------------------------------------------
+ * param: pNtHeaders, NT headers of a PE file
+ * param: rva, DWORD typed RVA to be converted
+ *
+ * return: DWORD offset
+ */
 DWORD rva_to_offset(PIMAGE_NT_HEADERS pNtHeaders, DWORD rva) {
   PIMAGE_SECTION_HEADER section = IMAGE_FIRST_SECTION(pNtHeaders);
 
