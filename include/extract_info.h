2b56d48635f529832ec0954e46ea561f327f8434
AdiKsOnDev <adilalizade13@gmail.com>
2024-06-09 12:20:00 +0400

    refactor: Moved system calls extraction

--- include/extract_info.h
+++ include/extract_info.h
@@ -15,17 +15,6 @@
  */
 void extract_imported_dlls(PBYTE pBase, PIMAGE_NT_HEADERS pNtHeaders);
 
-/* Given pBase and Headers of a PE file,
- * extracts the system calls utilized in the 
- * executable. Uses capstone library
- * -------------------------------------------
- * param: pbase,
- * param: pNtHeaders, Headers of a PE file
- *
- * return: void
- */ 
-void extract_system_calls(PBYTE pBase, PIMAGE_NT_HEADERS pNtHeaders);
-
 /* Given the Headers of a PE file, extracts and prints out
  * crucial system information, such as OS, Subsystem, etc.
  * -------------------------------------------
