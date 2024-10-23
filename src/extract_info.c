242dddc3896b2c2caac56baa2834ea8b852381b5
AdiKsOnDev <adilalizade13@gmail.com>
2024-10-23 05:45:57 +0400

    refactor: Code formatting

--- src/extract_info.c
+++ src/extract_info.c
@@ -1,7 +1,7 @@
 #include "../include/extract_info.h"
 #include "../include/pe_analyze.h"
 
-
+// DOS Header extraction, check the header file for documentation
 void extract_DOS_header_info(PIMAGE_DOS_HEADER pDosHeader) {
   printf("e_magic:    %d\n", pDosHeader->e_magic);
   printf("e_cblp:     %d\n", pDosHeader->e_cblp);
@@ -26,31 +26,26 @@ void extract_DOS_header_info(PIMAGE_DOS_HEADER pDosHeader) {
   return;
 }
 
+// Imported DLLs extraction, check the header file for documentation
 void extract_imported_dlls(PBYTE pBase, PIMAGE_NT_HEADERS pNtHeaders) {
   DWORD importDirectoryRVA =
       pNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT]
           .VirtualAddress;
+
   if (importDirectoryRVA == 0) {
     printf("No import directory found.\n");
     return;
   }
 
   DWORD importDirectoryOffset = rva_to_offset(pNtHeaders, importDirectoryRVA);
-  if (importDirectoryOffset == 0) {
-    printf("Invalid import directory offset.\n");
-    return;
-  }
 
   PIMAGE_IMPORT_DESCRIPTOR pImportDesc =
       (PIMAGE_IMPORT_DESCRIPTOR)(pBase + importDirectoryOffset);
-  if (pImportDesc == NULL) {
-    printf("No import table found.\n");
-    return;
-  }
 
   printf("Imported DLLs:\n");
   while (pImportDesc->Name != 0) {
     DWORD nameOffset = rva_to_offset(pNtHeaders, pImportDesc->Name);
+
     if (nameOffset == 0) {
       printf("Invalid DLL name address.\n");
       break;
@@ -84,45 +79,68 @@ void extract_imported_dlls(PBYTE pBase, PIMAGE_NT_HEADERS pNtHeaders) {
   }
 }
 
-// Function to extract system information from PE headers
+// Optional Headers extraction, check the header file for documentation
 void extract_optional_headers(PIMAGE_NT_HEADERS pNtHeaders) {
-    printf("Magic: 0x%x\n", pNtHeaders->OptionalHeader.Magic);
-    printf("Major Linker Version: %d\n", pNtHeaders->OptionalHeader.MajorLinkerVersion);
-    printf("Minor Linker Version: %d\n", pNtHeaders->OptionalHeader.MinorLinkerVersion);
-    printf("Size of Code: 0x%x\n", pNtHeaders->OptionalHeader.SizeOfCode);
-    printf("Size of Initialized Data: 0x%x\n", pNtHeaders->OptionalHeader.SizeOfInitializedData);
-    printf("Size of Uninitialized Data: 0x%x\n", pNtHeaders->OptionalHeader.SizeOfUninitializedData);
-    printf("Address of Entry Point: 0x%x\n", pNtHeaders->OptionalHeader.AddressOfEntryPoint);
-    printf("Base of Code: 0x%x\n", pNtHeaders->OptionalHeader.BaseOfCode);
+  printf("Magic: 0x%x\n", pNtHeaders->OptionalHeader.Magic);
+  printf("Major Linker Version: %d\n",
+         pNtHeaders->OptionalHeader.MajorLinkerVersion);
+  printf("Minor Linker Version: %d\n",
+         pNtHeaders->OptionalHeader.MinorLinkerVersion);
+  printf("Size of Code: 0x%x\n", pNtHeaders->OptionalHeader.SizeOfCode);
+  printf("Size of Initialized Data: 0x%x\n",
+         pNtHeaders->OptionalHeader.SizeOfInitializedData);
+  printf("Size of Uninitialized Data: 0x%x\n",
+         pNtHeaders->OptionalHeader.SizeOfUninitializedData);
+  printf("Address of Entry Point: 0x%x\n",
+         pNtHeaders->OptionalHeader.AddressOfEntryPoint);
+  printf("Base of Code: 0x%x\n", pNtHeaders->OptionalHeader.BaseOfCode);
 
-    if (pNtHeaders->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR32_MAGIC) {
-        // Print BaseOfData for PE32 only
-        printf("Base of Data: 0x%x\n", pNtHeaders->OptionalHeader.BaseOfData);
-    }
+  if (pNtHeaders->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR32_MAGIC) {
+    // Print BaseOfData for PE32 only
+    printf("Base of Data: 0x%x\n", pNtHeaders->OptionalHeader.BaseOfData);
+  }
 
-    printf("Image Base: 0x%llx\n", pNtHeaders->OptionalHeader.ImageBase);  // 64-bit for PE32+ or 32-bit for PE32
-    printf("Section Alignment: 0x%x\n", pNtHeaders->OptionalHeader.SectionAlignment);
-    printf("File Alignment: 0x%x\n", pNtHeaders->OptionalHeader.FileAlignment);
-    printf("Major OS Version: %d\n", pNtHeaders->OptionalHeader.MajorOperatingSystemVersion);
-    printf("Minor OS Version: %d\n", pNtHeaders->OptionalHeader.MinorOperatingSystemVersion);
-    printf("Major Image Version: %d\n", pNtHeaders->OptionalHeader.MajorImageVersion);
-    printf("Minor Image Version: %d\n", pNtHeaders->OptionalHeader.MinorImageVersion);
-    printf("Major Subsystem Version: %d\n", pNtHeaders->OptionalHeader.MajorSubsystemVersion);
-    printf("Minor Subsystem Version: %d\n", pNtHeaders->OptionalHeader.MinorSubsystemVersion);
-    printf("Win32 Version Value: 0x%x\n", pNtHeaders->OptionalHeader.Win32VersionValue);  // Should be 0
-    printf("Size of Image: 0x%x\n", pNtHeaders->OptionalHeader.SizeOfImage);
-    printf("Size of Headers: 0x%x\n", pNtHeaders->OptionalHeader.SizeOfHeaders);
-    printf("Checksum: 0x%x\n", pNtHeaders->OptionalHeader.CheckSum);
-    printf("Subsystem: 0x%x\n", pNtHeaders->OptionalHeader.Subsystem);
-    printf("DLL Characteristics: 0x%x\n", pNtHeaders->OptionalHeader.DllCharacteristics);
-    printf("Size of Stack Reserve: 0x%llx\n", pNtHeaders->OptionalHeader.SizeOfStackReserve);  // 64-bit for PE32+
-    printf("Size of Stack Commit: 0x%llx\n", pNtHeaders->OptionalHeader.SizeOfStackCommit);   // 64-bit for PE32+
-    printf("Size of Heap Reserve: 0x%llx\n", pNtHeaders->OptionalHeader.SizeOfHeapReserve);   // 64-bit for PE32+
-    printf("Size of Heap Commit: 0x%llx\n", pNtHeaders->OptionalHeader.SizeOfHeapCommit);     // 64-bit for PE32+
-    printf("Loader Flags: 0x%x\n", pNtHeaders->OptionalHeader.LoaderFlags);
-    printf("Number of Rva and Sizes: 0x%x\n", pNtHeaders->OptionalHeader.NumberOfRvaAndSizes);
+  printf("Image Base: 0x%llx\n",
+         pNtHeaders->OptionalHeader
+             .ImageBase); // 64-bit for PE32+ or 32-bit for PE32
+  printf("Section Alignment: 0x%x\n",
+         pNtHeaders->OptionalHeader.SectionAlignment);
+  printf("File Alignment: 0x%x\n", pNtHeaders->OptionalHeader.FileAlignment);
+  printf("Major OS Version: %d\n",
+         pNtHeaders->OptionalHeader.MajorOperatingSystemVersion);
+  printf("Minor OS Version: %d\n",
+         pNtHeaders->OptionalHeader.MinorOperatingSystemVersion);
+  printf("Major Image Version: %d\n",
+         pNtHeaders->OptionalHeader.MajorImageVersion);
+  printf("Minor Image Version: %d\n",
+         pNtHeaders->OptionalHeader.MinorImageVersion);
+  printf("Major Subsystem Version: %d\n",
+         pNtHeaders->OptionalHeader.MajorSubsystemVersion);
+  printf("Minor Subsystem Version: %d\n",
+         pNtHeaders->OptionalHeader.MinorSubsystemVersion);
+  printf("Win32 Version Value: 0x%x\n",
+         pNtHeaders->OptionalHeader.Win32VersionValue); // Should be 0
+  printf("Size of Image: 0x%x\n", pNtHeaders->OptionalHeader.SizeOfImage);
+  printf("Size of Headers: 0x%x\n", pNtHeaders->OptionalHeader.SizeOfHeaders);
+  printf("Checksum: 0x%x\n", pNtHeaders->OptionalHeader.CheckSum);
+  printf("Subsystem: 0x%x\n", pNtHeaders->OptionalHeader.Subsystem);
+  printf("DLL Characteristics: 0x%x\n",
+         pNtHeaders->OptionalHeader.DllCharacteristics);
+  printf("Size of Stack Reserve: 0x%llx\n",
+         pNtHeaders->OptionalHeader.SizeOfStackReserve); // 64-bit for PE32+
+  printf("Size of Stack Commit: 0x%llx\n",
+         pNtHeaders->OptionalHeader.SizeOfStackCommit); // 64-bit for PE32+
+  printf("Size of Heap Reserve: 0x%llx\n",
+         pNtHeaders->OptionalHeader.SizeOfHeapReserve); // 64-bit for PE32+
+  printf("Size of Heap Commit: 0x%llx\n",
+         pNtHeaders->OptionalHeader.SizeOfHeapCommit); // 64-bit for PE32+
+  printf("Loader Flags: 0x%x\n", pNtHeaders->OptionalHeader.LoaderFlags);
+  printf("Number of Rva and Sizes: 0x%x\n",
+         pNtHeaders->OptionalHeader.NumberOfRvaAndSizes);
 }
 
+// A helper function to list all files in a given directory, check the header
+// file for documentation
 void listFiles(int verbose, const char *directory) {
   WIN32_FIND_DATA findFileData;
   HANDLE hFind = INVALID_HANDLE_VALUE;
@@ -132,9 +150,10 @@ void listFiles(int verbose, const char *directory) {
   hFind = FindFirstFile(searchPath, &findFileData);
 
   if (hFind == INVALID_HANDLE_VALUE) {
-    printf("Invalid file handle. Error is %u\n", GetLastError());
+    printf("Invalid file handle.));
     printf("Make sure the directory path is correct and you have the necessary "
            "permissions.\n");
+
     return;
   } else {
     printf("Listing files in directory: %s\n", directory);
@@ -153,9 +172,7 @@ void listFiles(int verbose, const char *directory) {
 
       analyze_pe_file(filePath, verbose);
     } while (FindNextFile(hFind, &findFileData) != 0);
-    if (GetLastError() != ERROR_NO_MORE_FILES) {
-      printf("FindNextFile error. Error is %u\n", GetLastError());
-    }
+
     FindClose(hFind);
   }
 }
