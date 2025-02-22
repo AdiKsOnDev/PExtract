f06b6ee3c51646b1b37f1d3682840ba7f29b902b
AdiKsOnDev <adilalizade13@gmail.com>
2025-02-22 04:14:01 +0400

    fix: fopen() before accessing the json file

--- src/pe_analyze.c
+++ src/pe_analyze.c
@@ -74,6 +74,7 @@ void analyze_pe_file(char *pe_path, int verbose, int silent, char *output) {
   if (!silent) {
     print_DOS_header_info(pDosHeader);
     print_imported_dlls((PBYTE)pBase, pNtHeaders);
+
     if (verbose) {
       file = fopen(pe_path, "rb");
       print_section_names(pDosHeader, pNtHeaders, file);
@@ -83,8 +84,10 @@ void analyze_pe_file(char *pe_path, int verbose, int silent, char *output) {
   }
 
   if (strcmp(output, "") != 0) {
+    json_file = fopen(output, "a");
     DOS_header_to_json(pDosHeader, json_file);
     imported_dlls_to_json(pBase, pNtHeaders, json_file);
+
     if (verbose) {
       file = fopen(pe_path, "rb");
       section_names_to_json(pDosHeader, pNtHeaders, file, json_file);
